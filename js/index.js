f06054f977878b092b137f2d831108db7cc5982d
liyrmm <574312717@qq.com>
2019-03-19 08:45:18 +0800

    ggg

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 window.onload = function () {
 
-// 北京移入移出效果
-let bj=document.querySelector(".body-left-right")
-let bjSlide=document.querySelector(".bjSlide")
-bj.onmouseenter = function () {
-    bjSlide.style.display = "block";
-}
-bj.onmouseleave = function () {
-    bjSlide.style.display = "none";
-}
+    // 北京移入移出效果
+    let bj = document.querySelector(".body-left-right")
+    let bjSlide = document.querySelector(".bjSlide")
+    bj.onmouseenter = function () {
+        bjSlide.style.display = "block";
+    }
+    bj.onmouseleave = function () {
+        bjSlide.style.display = "none";
+    }
     //我的京东移入移出效果
     let myjd = document.querySelector(".myjd");
     let loginSlide1 = document.querySelector(".loginSlide1");
@@ -42,36 +42,501 @@ bj.onmouseleave = function () {
     }
 
     // 网站导航移入移出效果
-    let website=document.querySelector(".website");
-    let loginSlide4=document.querySelector(".loginSlide4");
-    website.onmouseenter=function(){
-        loginSlide4.style.display="block";
+    let website = document.querySelector(".website");
+    let loginSlide4 = document.querySelector(".loginSlide4");
+    website.onmouseenter = function () {
+        loginSlide4.style.display = "block";
     }
-    website.onmouseleave=function(){
-        loginSlide4.style.display="none";
+    website.onmouseleave = function () {
+        loginSlide4.style.display = "none";
     }
 
     // 手机京东移入移出效果
-    let mobile=document.querySelector(".mobile");
-    let mobileItems=document.querySelector(".mobile-items");
-    mobile.onmouseenter=function(){
-        mobileItems.style.display="block";
+    let mobile = document.querySelector(".mobile");
+    let mobileItems = document.querySelector(".mobile-items");
+    mobile.onmouseenter = function () {
+        mobileItems.style.display = "block";
     }
-    mobile.onmouseleave=function(){
-        mobileItems.style.display="none";
+    mobile.onmouseleave = function () {
+        mobileItems.style.display = "none";
     }
 
     // 轮播图列表移入移出效果
-    let bannerListItem=document.querySelectorAll(".mainbody-left-list-item")
-    let bannerSlide=document.querySelectorAll(".bannerSlide")
+    let bannerListItem = document.querySelectorAll(".mainbody-left-list-item")
+    let bannerSlide = document.querySelectorAll(".bannerSlide")
     // console.dir(bannerListItem)
     // console.dir(bannerSlide)
-    bannerListItem.forEach(function(v,i){
-        v.onmouseenter=function(){
-            bannerSlide[i].style.display="block"
+    bannerListItem.forEach(function (v, i) {
+        v.onmouseenter = function () {
+            bannerSlide[i].style.display = "block"
         }
-        v.onmouseleave=function(){
-            bannerSlide[i].style.display="none"
+        v.onmouseleave = function () {
+            bannerSlide[i].style.display = "none"
         }
     })
+
+    // 轮播图轮播
+    let index = 0;
+    let pages = document.querySelectorAll(".mainbody-body2-img a")
+    let banner = document.querySelector(".mainbody-body2-img")
+    let pre = document.querySelector(".mainbody-body2-button-left")
+    let next = document.querySelector(".mainbody-body2-button-right")
+    let dots = document.querySelectorAll(".mainbody-body2-dots .mainbody-body2-dot")
+    banner.onmouseenter = function () {
+        clearInterval(t)
+    }
+    banner.onmouseleave = function () {
+        t = setInterval(run, 3000)
+    }
+    function run(status = "next") {
+        if (status == "next") {
+            index += 1
+        } else if (status = "pre") {
+            index -= 1
+        }
+        if (index > 7) {
+            index = 0;
+        }
+        if (index < 0) {
+            index = 7
+        }
+        pages.forEach(function (item, i) {
+            item.classList.remove("active")
+        })
+        pages[index].classList.add("active")
+
+        dots.forEach(function (item, i) {
+            item.classList.remove("active")
+        })
+        dots[index].classList.add("active")
+    }
+
+    let t = setInterval(run, 3000)
+    next.onclick = function () {
+        run("next")
+    }
+    pre.onclick = function () {
+        run("pre")
+    }
+    // 轮播点点击效果
+    dots.forEach(function (item, i) {
+        item.onmouseenter = function () {                     //item就是获取到的div
+            dots.forEach(function (item) {
+                item.classList.remove("active")
+            })
+            item.classList.add("active")
+            pages.forEach(function (item) {
+                item.classList.remove("active")
+            })
+            pages[i].classList.add("active")
+        }
+    })
+
+
+
+    // 京东秒杀右边无缝轮播
+    let index1 = 0
+    let page = document.querySelectorAll(".sk-right-imgs a")
+    let bigpage = document.querySelector(".sk-right-imgs")
+    let dot = document.querySelectorAll(".sk-right-dot")
+    let prev = 0;
+    let now = 0;
+
+    page.forEach(function (item, i) {
+        if (i == 0) {
+            return;
+        }
+        item.style.left = "180px";
+    })
+    // 切换
+    function run1(status = 'next') {
+        if (status == "next") {
+            now += 1;
+            index1 += 1
+
+            if (now > 1) {
+                now = 0;
+            }
+            if (index1 > 1) {
+                index1 = 0;
+            }
+            page[now].style.left = '180px';
+            animate(page[now], { 'left': 0 });
+            animate(page[prev], { 'left': -180 });
+        } else if (status == "pre") {
+            now -= 1;
+            if (now < 0) {
+                now = 1;
+            }
+            page[now].style.left = '-180px';
+            animate(page[now], { 'left': 0 });
+            animate(page[prev], { 'left': 180 });
+        }
+        prev = now;
+        dot.forEach(function (item, i) {
+            item.classList.remove("active")
+        })
+        dot[now].classList.add("active")
+    }
+
+    let t1 = setInterval(run1, 3000);
+    // 移入停止
+    bigpage.onmouseenter = function () {
+        clearInterval(t1)
+    }
+    bigpage.onmouseleave = function () {
+        t1 = setInterval(run1, 3000)
+    }
+    // 轮播点点击效果
+    dot.forEach(function (item, i) {
+        item.onmouseenter = function () {
+            if (i > now) {
+                page[i].style.left = '180px';
+                animate(page[i], { 'left': 0 });
+                animate(page[prev], { 'left': -180 });
+                dot.forEach(function (item) {
+                    item.classList.remove("active")
+                })
+                dot[i].classList.add("active")
+                prev = i;
+                now = i;
+            } else {
+                page[i].style.left = '-180px';
+                animate(page[i], { 'left': 0 });
+                animate(page[prev], { 'left': 180 });
+                dot.forEach(function (item) {
+                    item.classList.remove("active")
+                })
+                dot[i].classList.add("active")
+                prev = i;
+                now = i;
+            }
+        }
+    })
+
+
+    // 会买专辑无缝轮播
+    let index2 = 0
+    let page2 = document.querySelectorAll(".dailyold-bds .dailyold-bd")
+    let bigpage2 = document.querySelector(".dailyold-bds")
+    let dot2 = document.querySelectorAll(".core-dailyold .special-dots .slider-dotr")
+    let prev2 = 0;
+    let now2 = 0;
+    let pre2 = document.querySelector(".core-dailyold .core-top-body .butl")
+    let next2 = document.querySelector(".core-dailyold .core-top-body .butr")
+
+    page2.forEach(function (item, i) {
+        if (i == 0) {
+            return;
+        }
+        item.style.left = "350px";
+    })
+    // 切换
+    function run2(status = 'next') {
+        if (status == "next") {
+            now2 += 1;
+            index2 += 1
+            if (now2 > 2) {
+                now2 = 0;
+            }
+            if (index2 > 2) {
+                index2 = 0;
+            }
+            page2[now2].style.left = '350px';
+            animate(page2[now2], { 'left': 0 });
+            animate(page2[prev2], { 'left': -350 });
+        } else if (status == "pre") {
+            now2 -= 1;
+            if (now2 < 0) {
+                now2 = 2;
+            }
+            page2[now2].style.left = '-350px';
+            animate(page2[now2], { 'left': 0 });
+            animate(page2[prev2], { 'left': 350 });
+        }
+        prev2 = now2;
+        dot2.forEach(function (item, i) {
+            item.classList.remove("active")
+        })
+        dot2[now2].classList.add("active")
+    }
+
+    let t2 = setInterval(run2, 3000);
+
+    // 移入停止
+    bigpage2.onmouseenter = function () {
+        clearInterval(t2)
+    }
+    bigpage2.onmouseleave = function () {
+        t2 = setInterval(run2, 3000)
+    }
+    pre2.onclick = function () {
+        run2("pre")
+    }
+    next2.onclick = function () {
+        run2()
+    }
+    // 轮播点点击效果
+    // 轮播点点击事件
+    dot2.forEach(function (item, i) {
+        item.onmouseenter = function () {
+            if (i > now) {
+                page2[i].style.left = '350px';
+                animate(page2[i], { 'left': 0 });
+                animate(page2[prev2], { 'left': -350 });
+                dot2.forEach(function (item) {
+                    item.classList.remove("active")
+                })
+                dot2[i].classList.add("active")
+                prev2 = i;
+                now2 = i;
+            } else {
+                page2[i].style.left = '-350px';
+                animate(page2[i], { 'left': 0 });
+                animate(page2[prev2], { 'left': 350 });
+                dot2.forEach(function (item) {
+                    item.classList.remove("active")
+                })
+                dot2[i].classList.add("active")
+                prev2 = i;
+                now2 = i;
+            }
+        }
+    })
+
+
+    // 觅me无缝轮播
+    let index3 = 0
+    let page3 = document.querySelectorAll(".mimebds .dailyold-bd")
+    let bigpage3 = document.querySelector(".mimebds")
+    let dot3 = document.querySelectorAll(".core-top .special-dots3 .slider-dotr")
+    let prev3 = 0;
+    let now3 = 0;
+    let pre3 = document.querySelector(".core-top .core-top-body .butl")
+    let next3 = document.querySelector(".core-top .core-top-body .butr")
+
+    page3.forEach(function (item, i) {
+        if (i == 0) {
+            return;
+        }
+        item.style.left = "350px";
+    })
+    // 切换
+    function run3(status = 'next') {
+        if (status == "next") {
+            now3 += 1;
+            index3 += 1
+            if (now3 > 1) {
+                now3 = 0;
+            }
+            if (index3 > 1) {
+                index3 = 0;
+            }
+            page3[now3].style.left = '350px';
+            animate(page3[now3], { 'left': 0 });
+            animate(page3[prev3], { 'left': -350 });
+        } else if (status == "pre") {
+            now3 -= 1;
+            if (now3 < 0) {
+                now3 = 1;
+            }
+            page3[now3].style.left = '-350px';
+            animate(page3[now3], { 'left': 0 });
+            animate(page3[prev3], { 'left': 350 });
+        }
+        prev3 = now3;
+        dot3.forEach(function (item, i) {
+            item.classList.remove("active")
+        })
+        dot3[now3].classList.add("active")
+    }
+
+    let t3 = setInterval(run3, 3000);
+
+    // 移入停止
+    bigpage3.onmouseenter = function () {
+        clearInterval(t3)
+    }
+    bigpage3.onmouseleave = function () {
+        t3 = setInterval(run3, 3000)
+    }
+    pre3.onclick = function () {
+        run3("pre")
+    }
+    next3.onclick = function () {
+        run3()
+    }
+    // 轮播点点击效果
+    dot3.forEach(function (item, i) {
+        item.onmouseenter = function () {
+            if (i > now) {
+                page3[i].style.left = '350px';
+                animate(page3[i], { 'left': 0 });
+                animate(page3[prev3], { 'left': -350 });
+                dot3.forEach(function (item) {
+                    item.classList.remove("active")
+                })
+                dot3[i].classList.add("active")
+                prev3 = i;
+                now3 = i;
+            } else {
+                page3[i].style.left = '-350px';
+                animate(page3[i], { 'left': 0 });
+                animate(page3[prev3], { 'left': 350 });
+                dot3.forEach(function (item) {
+                    item.classList.remove("active")
+                })
+                dot3[i].classList.add("active")
+                prev3 = i;
+                now3 = i;
+            }
+        }
+    })
+
+
+
+    // 领券中心轮播点移入移出效果
+    let page4 = document.querySelectorAll(".core-last .dailyold-bd")
+    let dot4 = document.querySelectorAll(".special-dots4 .slider-dotr")
+    let prev4 = 0;
+    let now4 = 0;
+    page4.forEach(function (item, i) {
+        if (i == 0) {
+            return;
+        }
+        item.style.left = "350px";
+    })
+    dot4.forEach(function (item, i) {
+        item.classList.remove("active")
+    })
+    dot4[now4].classList.add("active")
+    // 轮播点点击效果
+    dot4.forEach(function (item, i) {
+        item.onmouseenter = function () {
+            if (i > now4) {
+                page4[i].style.left = '350px';
+                animate(page4[i], { 'left': 0 });
+                animate(page4[prev4], { 'left': -350 });
+                dot4.forEach(function (item) {
+                    item.classList.remove("active")
+                })
+                dot4[i].classList.add("active")
+                prev4 = i;
+                now4 = i;
+            } else {
+                page4[i].style.left = '-350px';
+                animate(page4[i], { 'left': 0 });
+                animate(page4[prev4], { 'left': 350 });
+                dot4.forEach(function (item) {
+                    item.classList.remove("active")
+                })
+                dot4[i].classList.add("active")
+                prev4 = i;
+                now4 = i;
+            }
+        }
+    })
+
+
+
+    // 排行榜轮播点移入移出效果
+    let page5 = document.querySelectorAll(".milk-bd")
+    let dot5 = document.querySelectorAll(".special-dots5 .slider-dotr")
+    let prev5 = 0;
+    let now5 = 0;
+    page5.forEach(function (item, i) {
+        if (i == 0) {
+            return;
+        }
+        item.style.left = "350px";
+    })
+    dot5.forEach(function (item, i) {
+        item.classList.remove("active")
+    })
+    dot5[now5].classList.add("active")
+    // 轮播点点击效果
+    dot5.forEach(function (item, i) {
+        item.onmouseenter = function () {
+            if (i > now5) {
+                page5[i].style.left = '350px';
+                animate(page5[i], { 'left': 0 });
+                animate(page5[prev5], { 'left': -350 });
+                dot5.forEach(function (item) {
+                    item.classList.remove("active")
+                })
+                dot5[i].classList.add("active")
+                prev5 = i;
+                now5 = i;
+            } else {
+                page5[i].style.left = '-350px';
+                animate(page5[i], { 'left': 0 });
+                animate(page5[prev5], { 'left': 350 });
+                dot5.forEach(function (item) {
+                    item.classList.remove("active")
+                })
+                dot5[i].classList.add("active")
+                prev5 = i;
+                now5 = i;
+            }
+        }
+    })
+
+
+    // 京东秒杀无缝切换
+    let page6 = document.querySelectorAll(".sk-list-items")
+    let prev6 = 0;
+    let now6 = 0;
+    let pre6 = document.querySelector(".sk-buttonl")
+    let next6 = document.querySelector(".sk-buttonr")
+
+    page6.forEach(function (item, i) {
+        if (i == 0) {
+            return;
+        }
+        item.style.left = "800px";
+    })
+
+    // 切换
+    // if (status == "next") {
+    //     now6 += 1;
+    //     if (now6 > 4) {
+    //         now6 = 0;
+    //     }
+
+    //     page6[now6].style.left = '800px';
+    //     animate(page6[now6], { 'left': 0 });
+    //     animate(page6[prev6], { 'left': -800 });
+    // } else if (status == "pre") {
+    //     now6 -= 1;
+    //     if (now6 < 0) {
+    //         now6 = 4;
+    //     }
+    //     page6[now6].style.left = '-800px';
+    //     animate(page6[now6], { 'left': 0 });
+    //     animate(page6[prev6], { 'left': 800 });
+    // }
+    // prev6 = now6;
+
+
+//     pre6.onclick = function () {
+//         now6 -= 1;
+//         if (now6 < 0) {
+//             now6 = 4;
+//         }
+//         page6[now6].style.left = '-800px';
+//         animate(page6[now6], { 'left': 0 });
+//         animate(page6[prev6], { 'left': 800 });
+//     }
+//     next6.onclick = function () {
+//         now6 += 1;
+//         if (now6 > 4) {
+//             now6 = 0;
+//         }
+
+//         page6[now6].style.left = '800px';
+//         animate(page6[now6], { 'left': 0 });
+//         animate(page6[prev6], { 'left': -800 });
+//     }
+// prev6=now6
+
 }
